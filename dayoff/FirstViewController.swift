ff2236e3453e1f9b52354aa637e194bd91db25f5
Bela Mate Barandi <kismansonka@gmail.com>
2018-04-29 00:41:10 +0200

    Initial Commit

---
+++ dayoff/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  dayoff
+//
+//  Created by Bela Mate Barandi on 4/29/18.
+//  Copyright © 2018 Bela Mate Barandi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ dayoff/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  dayoff
+//
+//  Created by Bela Mate Barandi on 4/29/18.
+//  Copyright © 2018 Bela Mate Barandi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
